3255a1b461a893013dfa9304d729438aaedcd246
Mark Unger <32203932+markunger@users.noreply.github.com>
2018-01-28 15:34:56 -0800

    Create morelines-2.js

---
+++ morelines-2.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(340, 340);
+}
+//Assignment (a)
+
+function draw() {
+  line(170, 170, random(width), random(height));
+}
+
+//Assignment (b)
+
+function draw() {
+  line(random(width), random(height), 340, 0);
+}
+
+//Assignment (c)
+

e8a1b2eb966ed57edf2cc9a52766c845b78b2087
Mark Unger <32203932+markunger@users.noreply.github.com>
2018-01-29 12:59:49 -0800

    Update fadingcircles-3.js

--- fadingcircles-3.js
+++ fadingcircles-3.js
@@ -1,9 +1,9 @@
 background(255);
-colorMode(HSB, 8, 100, 100, 1);
+colorMode(HSB);
 noStroke();
 
 while(true) {
   background(0, 0, 255, 0.1);
-  fill(random(360), 255, 255);
-  ellipse(random(width),random(height), random(80, 40));
+  fill(random(120), 255, 255);
+  ellipse(random(width),random(height), random(80, 60));
 }
